b8043d410aa26d850a77790588460630a790c2ea
anton_penkov <anton.penkov1@gmail.com>
2024-03-18 15:32:59 +0100

    Initial Commit

---
+++ CreativityHomework/CreativityHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativityHomeworkApp.swift
+//  CreativityHomework
+//
+//  Created by Антон Пеньков on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CreativityHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
